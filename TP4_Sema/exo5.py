9ac2fef109b61f7c7d69e32fe240c78307d56f89
lucanchling <luc.anchling@cpe.fr>
2021-06-02 20:31:53 +0200

    ZZZzzzz

---
+++ TP4_Sema/exo5.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 09:17:39 2019
+
+@author: noemie.rolland
+"""
+
+import multiprocessing as mp
+from multiprocessing import Process, Value
+import time ,sys
+
+# EXERCICE 5 
+# Exemple 2 Process
+
+#def processusP1(sem1):
+#        sem1.acquire()
+#        print("Je suis P1")
+#        time.sleep(1)
+#        sem2.release()
+#        sem1.acquire()
+#        rdv1()
+#    
+#def processusP2(sem2):
+#        sem2.acquire()
+#        print("Je suis P2")
+#        time.sleep(1)
+#        sem1.release()
+#        sem1.release()
+#        sem1.acquire()
+#        rdv2()
+#        
+#def rdv1():
+#    print("rdv1")
+#    
+#def rdv2():
+#    print("rdv2")
+#        
+#sem1=mp.Semaphore(1)  
+#sem2=mp.Semaphore(0) 
+#P1=mp.Process(target=processusP1, args=(sem1,))
+#P2=mp.Process(target=processusP2, args=(sem2,))
+#
+#P1.start()
+#P2.start()
+#P1.join()
+#P2.join()
+
+#Exemple 3 Process
+
+def processusP1(sem1):
+
+        print("Je suis P1")
+        time.sleep(3)
+        sem1.release() #lache un jeton pour P2
+        sem1.release() #lache un jeton pour P3
+        sem2.acquire() #vérifie que P2 s'est déroulé
+        sem3.acquire() #vérifie que P3 s'est déroulé
+        rdv1()
+    
+def processusP2(sem2):
+
+        print("Je suis P2")
+        time.sleep(2)
+        sem2.release() #lache un jeton pour P1
+        sem2.release() #lache un jeton pour P3
+        sem1.acquire() #vérifie que P1 s'est déroulé
+        sem3.acquire() #vérifie que P3 s'est déroulé
+        rdv2()
+        
+def processusP3(sem3):
+
+        print("Je suis P3")
+        time.sleep(1)
+        sem3.release() #lache un jeton pour P1
+        sem3.release() #lache un jeton pour P2
+        sem1.acquire() #vérifie que P3 s'est déroulé
+        sem2.acquire() #vérifie que P2 s'est déroulé
+        rdv3()
+        
+def rdv1():
+    print("rdv1")
+    
+def rdv2():
+    print("rdv2")
+    
+def rdv3():
+    print("rdv3")
+        
+sem1=mp.Semaphore(0)  #semaphore contenant les jetons créés par P1
+sem2=mp.Semaphore(0) #semaphore contenant les jetons créés par P2
+sem3=mp.Semaphore(0) #semaphore contenant les jetons créés par P3
+P1=mp.Process(target=processusP1, args=(sem1,))
+P2=mp.Process(target=processusP2, args=(sem2,))
+P3=mp.Process(target=processusP3, args=(sem3,))
+
+P1.start()
+P2.start()
+P3.start()
+P1.join()
+P2.join()
+P3.join()
+
+# ERXERCICE 6
+
---
+++ TP4_Sema/exo6.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 09:41:08 2019
+
+@author: noemie.rolland
+"""
+
+import multiprocessing as mp
+from multiprocessing import Process, Value
+import time ,sys
+
+def emetteur(semR,semE):
+    mutex.acquire() #on protege pour qu'un seul emetteur accede aux 2 jetons recpteur
+    print ("E franchi mutex")
+    semR.acquire() # verif presence de deux recepteurs
+    semR.acquire()  # verif presence de deux recepteurs
+    print("E débloqué")
+    semE.release() #creation jeton pour un recepteur
+    semE.release() #creation jeton pour un recepteur
+    mutex.release() #on reouvre le verrou
+    
+def recepteur(semR,semE):
+    semR.release() #chaque recepteur créer un jeton 
+    print("R la")
+    semE.acquire() #chaque recepteur recupere un jeton emetteur
+    print("R débloqué")
+    
+    
+
+
+
+   
+ 
+semE=mp.Semaphore(0)  #semaphore dans lequel l'emetteur met ses jetons
+semR=mp.Semaphore(0) #semaphore dans lequel le recepteur met ses jetons
+mutex=mp.Lock()
+lp=[]
+for arg in sys.argv :
+    if arg=="R":
+        R=mp.Process(target=recepteur, args=(semR,semE,))
+        lp.append(R)
+        R.start()
+    if arg=="E":
+        E=mp.Process(target=emetteur, args=(semR,semE,))
+        lp.append(E)
+        E.start()
+        
+for p in lp :
+    p.join()
